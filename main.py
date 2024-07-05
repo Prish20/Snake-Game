9107a0be587e41916f718e5d33c3d511f2f465bc
Prish20 <prish@students.uonbi.ac.ke>
2024-07-05 17:57:55 +0300

    Detect collisions with the tail

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from scoreboard import Scoreboard
 import time
 
 screen = Screen()
-screen.setup(width=600, height=600)
+screen.setup(width=996, height=912)
 screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
@@ -29,11 +29,18 @@ while game_is_on:
     # Detect collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.extend()
         scoreboard.increase_score()
 
     # Detect collision with wall
-    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+    if snake.head.xcor() > 956 or snake.head.xcor() < -912 or snake.head.ycor() > 956 or snake.head.ycor() < -912:
         game_is_on = False
         scoreboard.game_over()
+
+    # Detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 screen.exitonclick()
